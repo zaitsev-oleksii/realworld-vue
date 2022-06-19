0647a057f1be25b25e1ec5d0f2343978b6bc6c9c
Oleksii <zaytsev961st@gmail.com>
2022-06-19 22:23:16 +0300

    cond display for navbar

--- src/layout/NavbarLayout.vue
+++ src/layout/NavbarLayout.vue
@@ -7,30 +7,41 @@
           <!-- Add "active" class when you're on that page" -->
           <router-link to="/" class="nav-link active">Home</router-link>
         </li>
-        <li class="nav-item">
-          <router-link to="/editor" class="nav-link">
-            <i class="ion-compose"></i>&nbsp;New Article
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/settings" class="nav-link">
-            <i class="ion-gear-a"></i>&nbsp;Settings
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">Sign in</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">Sign up</router-link>
-        </li>
+        <template v-if="isLoggedIn">
+          <li class="nav-item">
+            <router-link to="/editor" class="nav-link">
+              <i class="ion-compose"></i>&nbsp;New Article
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/settings" class="nav-link">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </router-link>
+          </li>
+        </template>
+        <template v-if="!isLoggedIn">
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">Sign up</router-link>
+          </li>
+        </template>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
+import { useStore } from "vuex";
+
 export default {
   name: "NavbarLayout",
-  setup() {}
+  setup() {
+    const store = useStore();
+    const isLoggedIn = store.getters.isLoggedIn;
+
+    return { isLoggedIn };
+  }
 };
 </script>
