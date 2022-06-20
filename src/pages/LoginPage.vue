1b9904af16878fd88a6c08247ba1ce18cd45e8f5
Oleksii <zaytsev961st@gmail.com>
2022-06-20 19:11:44 +0300

    template fix

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -3,7 +3,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
+          <h1 class="text-xs-center">Sign in</h1>
           <p class="text-xs-center">
             <a href="">Have an account?</a>
           </p>
@@ -26,7 +26,7 @@
               />
             </fieldset>
             <button type="submit" class="btn btn-lg btn-primary pull-xs-right">
-              Sign up
+              Sign in
             </button>
           </form>
         </div>
