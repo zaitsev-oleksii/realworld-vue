9362a95dafb8405d8a2d12bac5b54d476d2ed93a
Oleksii <zaytsev961st@gmail.com>
2022-06-24 18:34:35 +0300

    addTab and removeTab functions

--- src/composables/tabs.js
+++ src/composables/tabs.js
@@ -13,28 +13,56 @@ import { useStore } from "vuex";
 const useTabs = (tabsMeta) => {
   const store = useStore();
 
-  const accessibleTabs = (
-    !store.getters.isAuthorized
+  const accessibleTabs = ref(
+    (!store.getters.isAuthorized
       ? tabsMeta.filter((tab) => tab.requiresAuth !== true)
       : tabsMeta
-  ).map((tab) => ({
-    name: tab.name,
-    display: tab.display,
-    requiresAuth: tab.requiresAuth,
-    component: markRaw(tab.component),
-    props: tab.props
-  }));
+    ).map((tab) => ({
+      name: tab.name,
+      display: tab.display,
+      requiresAuth: tab.requiresAuth,
+      component: markRaw(tab.component),
+      props: tab.props
+    }))
+  );
 
-  const currentTab = ref(accessibleTabs[0]);
+  const currentTab = ref(accessibleTabs.value[0]);
 
   const currentTabComponent = computed(() => currentTab.value.component);
 
   const setCurrentTab = (newTabName) => {
-    const newTab = accessibleTabs.find((tab) => tab.name === newTabName);
+    const newTab = accessibleTabs.value.find((tab) => tab.name === newTabName);
     currentTab.value = newTab;
   };
 
-  return { accessibleTabs, currentTab, currentTabComponent, setCurrentTab };
+  const addTab = (newTabMeta) => {
+    const newTab =
+      !store.getters.isAuthorized && newTabMeta.requiresAuth === true
+        ? null
+        : {
+            name: newTabMeta.name,
+            display: newTabMeta.display,
+            requiresAuth: newTabMeta.requiresAuth,
+            component: markRaw(newTabMeta.component),
+            props: newTabMeta.props
+          };
+    if (newTab) accessibleTabs.value.push(newTab);
+  };
+
+  const removeTab = (tabName) => {
+    accessibleTabs.value = accessibleTabs.value.filter(
+      (tab) => tab.name !== tabName
+    );
+  };
+
+  return {
+    accessibleTabs,
+    currentTab,
+    currentTabComponent,
+    setCurrentTab,
+    addTab,
+    removeTab
+  };
 };
 
 export default useTabs;
